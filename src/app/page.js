3a57cbd0b768ff880b361b2c179329da437297ef
Nikola Komljenovic <nikola.komljenovic@flow.space>
2024-03-01 21:21:53 +0100

    initial structure of the project

---
+++ src/app/page.js
@@ -0,0 +1,65 @@
+'use client'
+
+import { useEffect, useState } from 'react';
+
+function WebToLeadForm() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+  });
+
+  useEffect(() => {
+    setFormData({
+      firstName: '',
+      lastName: '',
+    });
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await fetch(`${process.env.sfURL}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams(formData).toString(),
+    });
+    if (!response.ok) {
+      // Handle error
+      console.error('Submission failed');
+      return;
+    }
+    // Handle success
+    console.log('Submission successful');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="max-w-sm mx-auto">
+      <input type="hidden" name="oid" value={process.env.oid} />
+      <input type="hidden" name="retURL" value={process.env.retURL} />
+
+      <div className="mb-4">
+        <label htmlFor="firstName" className="block text-sm font-medium text-gray-700">First Name:</label>
+        <input type="text" id="firstName" name="firstName" value={formData.firstName} onChange={handleChange} required className="mt-1 p-2 block w-full border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500" />
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">Last Name:</label>
+        <input type="text" id="lastName" name="lastName" value={formData.lastName} onChange={handleChange} required className="mt-1 p-2 block w-full border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500" />
+      </div>
+
+      <button type="submit" className="w-full bg-indigo-600 text-white p-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Submit</button>
+    </form>
+  );
+}
+
+export default WebToLeadForm;
+
