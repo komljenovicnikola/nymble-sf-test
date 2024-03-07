8027ae13f8b1ef2bf4d85c43d63e8b4b1b9a1466
Nikola Komljenovic <nikola.komljenovic@flow.space>
2024-03-07 13:06:08 +0100

    revert changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,57 +1,11 @@
 'use client'
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 const LeadForm = () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [isSubmitted, setIsSubmitted] = useState(false);
-  const [gaClientId, setGaClientId] = useState('');
-
-  useEffect(() => {
-    // Function to get Google Analytics Client ID
-    const getGaClientId = () => {
-      const tracker = window.ga.getAll()[0];
-      if (tracker) {
-        setGaClientId(tracker.get('clientId'));
-      }
-    };
-
-    // Load Google Analytics script and get Client ID
-    const loadGaScript = () => {
-      if (!window.ga) {
-        (function(i, s, o, g, r, a, m) {
-          i['GoogleAnalyticsObject'] = r;
-          (i[r] =
-            i[r] ||
-            function() {
-              (i[r].q = i[r].q || []).push(arguments);
-            }),
-            (i[r].l = 1 * new Date());
-          (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-          a.async = 1;
-          a.src = g;
-          m.parentNode.insertBefore(a, m);
-        })(
-          window,
-          document,
-          'script',
-          'https://www.google-analytics.com/analytics.js',
-          'ga'
-        );
-        window.ga('create', 'G-YVMS31034B', 'auto');
-        window.ga('send', 'pageview');
-        getGaClientId();
-        console.log(gaClientId);
-      } else {
-        getGaClientId();
-      }
-    };
-
-    loadGaScript();
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -61,7 +15,6 @@ const LeadForm = () => {
       retURL: process.env.NEXT_PUBLIC_SALESFORCE_RETURL,
       first_name: firstName,
       last_name: lastName,
-      ga_client_id: gaClientId, // Appending Google Analytics Client ID
     };
 
     const response = await fetch('/api/proxy', {
